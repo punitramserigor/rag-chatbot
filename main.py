f0f2f11ed1bbfe445023edd4c30807092479c297
Punitram Joshi <pjoshi@serigor.com>
2024-07-09 13:23:02 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, jsonify
+import langchain.document_loaders
+from model1 import RAG
+from dotenv import load_dotenv
+import langchain
+
+langchain.debug=True
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route("/load_db", methods=["POST"])
+def load_db():
+    data: dict = request.get_json()
+    session_id = data.get("session_id")
+    file_path = data.get("file_path")
+    supported_formats = ["pptx", "docx", "pdf", "txt", "md"]
+    file_extension = file_path.split(".")[-1]
+    if file_path and session_id:
+        if file_extension in supported_formats:
+            # try:
+                rag_model = RAG(session_id=session_id)
+                rag_model.load_db(file_path)
+                return jsonify({"success": "Database loaded successfully"}), 200
+            # except Exception as e:
+            #     return jsonify({"error": str(e)}), 500
+        else:
+            return jsonify({"error": "Unsupported file type"}), 400
+    else:
+        return jsonify({"error": "Missing required parameters"}), 400
+
+
+@app.route("/doc_invoke", methods=["POST"])
+def doc_invoke():
+    data: dict = request.get_json()
+    session_id = data.get("session_id")
+    query = data.get("query")
+
+    if session_id and query:
+        # try:
+            rag_model = RAG(session_id=session_id)
+            answer = rag_model.invoke(query=query)
+            return jsonify({"answer": answer}), 200
+        # except Exception as e:
+        #     return jsonify({"error": str(e)}), 500
+    else:
+        return jsonify({"error": "Missing required parameters"}), 400
+
+
+@app.route("/delete_db", methods=["POST"])
+def delete_db():
+    data: dict = request.get_json()
+    session_id = data.get("session_id")
+
+    if session_id:
+        try:
+            rag_model = RAG(session_id=session_id)
+            rag_model.delete_db()
+            return jsonify({"success": "Database deleted successfully"}), 200
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+    else:
+        return jsonify({"error": "Missing required parameters"}), 400
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0")
---
+++ model.py
@@ -0,0 +1,144 @@
+from langchain_community.document_loaders import (
+    PyPDFLoader,
+    TextLoader,
+    UnstructuredMarkdownLoader,
+    Docx2txtLoader,
+    JSONLoader,
+)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains.question_answering import load_qa_chain
+from langchain.memory import ConversationBufferWindowMemory
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_milvus import Milvus
+from langchain_community.chat_message_histories import FileChatMessageHistory
+from langchain_core.prompts import PromptTemplate
+import os
+import json
+
+
+class RAG:
+    def __init__(self, session_id) -> None:
+        self.expr = f"session_id == '{session_id}'"
+        self.session_id = session_id
+        self.memory_file = f"{session_id}_memory.json"
+        self.embedding_function = OpenAIEmbeddings()
+        self.llm = ChatOpenAI(model="gpt-4o", temperature=0)
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=2000,
+            chunk_overlap=200,
+            separators=[
+                "\n\n",
+                "\n",
+                " ",
+                ".",
+                ",",
+                "",
+            ],
+        )
+        self.connection_args = {
+            "uri": os.getenv("ZILLIZ_CLOUD_URI"),
+            "username": os.getenv("ZILLIZ_CLOUD_USERNAME"),
+            "password": os.getenv("ZILLIZ_CLOUD_PASSWORD"),
+            "token": os.getenv("ZILLIZ_CLOUD_API_KEY"),
+            "secure": True,
+        }
+        self.prompt_template = """You are an AI Assistant having a conversation with a human.
+
+            You will be given the extracted parts of a long document as a context, and a question related to that context.
+
+            Use only and only the provided context to answer the question. DO NOT use your own knowledge base to answer the questions.
+
+            ### Context:
+            {context}
+
+            ### Previous Conversation:
+            {chat_history}
+
+            ### Query:
+            Human: {human_input}
+            AI:"""
+        prompt = PromptTemplate(
+            input_variables=["chat_history", "human_input", "context"],
+            template=self.prompt_template,
+        )
+        
+        self.chat_memory = FileChatMessageHistory(
+            file_path=self.memory_file
+        )
+
+        self.chain = load_qa_chain(
+            self.llm, chain_type="stuff", prompt=prompt
+        )
+
+    def load_conversation(self, file_path):
+        with open(file_path, 'r') as file:
+            conversation = json.load(file)
+            
+        # Get the latest 10 messages
+        latest_messages = conversation[-10:]
+
+        formatted_output = []
+        for message in latest_messages:
+            message_type = message['type']
+            content = message['data']['content']
+            formatted_output.append(f"{message_type}: {content}")
+        
+        return "\n".join(formatted_output)
+
+    def load_db(self, file_path: str):
+        success = self.ingest_documents(file_path=file_path)
+        return success
+
+    def ingest_documents(self, file_path):
+        if ".pdf" in file_path:
+            self.loader = PyPDFLoader(file_path)
+        if ".md" in file_path:
+            self.loader = UnstructuredMarkdownLoader(file_path)
+        if ".txt" in file_path:
+            self.loader = TextLoader(file_path)
+        if ".json" in file_path:
+            self.loader = JSONLoader(file_path)
+        if ".docx" in file_path:
+            self.loader = Docx2txtLoader(file_path)
+        self.document_list = self.loader.load_and_split(
+            text_splitter=self.text_splitter
+        )
+        for document in self.document_list:
+            document.metadata = {"session_id": self.session_id}
+
+        # Connect to Zilliz Cloud and create the vector store
+        self.vector_store = Milvus.from_documents(
+            documents=self.document_list,
+            embedding=self.embedding_function,
+            connection_args=self.connection_args
+        )
+        return True
+
+    def invoke(self, query: str):
+        self.vector_store = Milvus(
+            embedding_function=self.embedding_function,
+            connection_args=self.connection_args,
+        )
+        retriever = self.vector_store.as_retriever(
+            search_type="similarity"
+        )
+        retrieved_docs = retriever.invoke(query)
+        conversation = self.load_conversation(self.memory_file)
+        self.response = self.chain.invoke(
+            {"input_documents": retrieved_docs, "human_input": query, "chat_history":conversation},
+            return_only_outputs=True
+        )
+        self.chat_memory.add_user_message(query)
+        self.chat_memory.add_ai_message(self.response["output_text"])
+        return self.response["output_text"]
+
+    def delete_db(self):
+        self.vector_store = Milvus(
+            embedding_function=self.embedding_function,
+            connection_args=self.connection_args,
+        )
+        self.vector_store.delete(expr=self.expr)
+
+        # Delete the memory file
+        if os.path.exists(self.memory_file):
+            os.remove(self.memory_file)
